c1135d91e60d67f131bc1b72d29cad3c0b0ebde0
Raphael Arar <me@rarar.com>
2020-11-10 19:26:48 -0800

    Latest update

--- TransactionalEducationArduino.ino
+++ TransactionalEducationArduino.ino
@@ -1,9 +1,21 @@
+#include "Adafruit_Thermal.h"
+#include "SoftwareSerial.h"
+#include "kalogo_bw3.h"
+#define TX_PIN 6 // Arduino transmit  YELLOW WIRE  labeled RX on printer
+#define RX_PIN 5 // Arduino receive   GREEN WIRE   labeled TX on printer
+
+SoftwareSerial mySerial(RX_PIN, TX_PIN); // Declare SoftwareSerial obj first
+Adafruit_Thermal printer(&mySerial);     // Pass addr to printer constructor
+
 String readString;
+String startFPM, endFPM, masteryLevelChange, numAttempted, numCorrect, pointsEarned;
 String label;
 int count = 0;
 void setup() {
   Serial.begin(9600); // Starts the serial communication
-  pinMode(LED_BUILTIN, OUTPUT);
+  mySerial.begin(19200);
+  pinMode(7, OUTPUT); digitalWrite(7, LOW);
+  printer.begin();
 }
 void loop() {
   while (Serial.available()) {
@@ -11,42 +23,87 @@ void loop() {
     char c = Serial.read();
     readString += c;
     if (readString.length() > 0 && c == '\n') {
-      readString.replace("\n","");
+      readString.replace("\n", "");
       switch (count) {
         case 0:
-          label = "= handshake succes";
-          readString += label;
+          //            label = "= handshake succes";
+          //            readString += label;
           break;
         case 1:
-          label = " = endingFpmLevel";
-          readString += label;
+          // ending FPM
+          endFPM = readString;
           break;
         case 2:
-          label = " = masteryLevelChange";
-          readString += label;
+          // mastery level change
+          masteryLevelChange = readString;
           break;
         case 3:
-          label = " = numAttempted";
-          readString += label;
+          // number attempted
+          numAttempted = readString;
           break;
         case 4:
-          label = " = numCorrect";
-          readString += label;
+          // number correct
+          numCorrect = readString;
           break;
         case 5:
-          label = " = pointsEarned";
-          readString += label;
+          // points earned
+          pointsEarned = readString;
           break;
         case 6:
-          label = " = startingFpmLevel";
-          readString += label;
+          // starting FPM
+          startFPM = readString;
           break;
       }
-      Serial.println(readString);
-      Serial.println(count);
+      //        Serial.println(readString);
+      //        Serial.println(count);
+      //printer.println(readString);
       readString = "";
       count++;
-      if (count > 6) count = 1;
+      if (count > 6) {
+        printer.printBitmap(kalogo_bw3_width, kalogo_bw3_height, kalogo_bw3_data);
+        printer.println("");
+
+        printer.justify('L');
+        printer.setSize('S');
+        printer.boldOn();
+        printer.println(F("Ending Mastery Level"));
+        printer.boldOff();
+        printer.setSize('M');
+        printer.println(endFPM);
+        printer.println();
+        printer.boldOn();
+        printer.setSize('S');
+        printer.println(F("Mastery Level Change"));
+        printer.boldOff();
+        printer.setSize('M');
+        printer.println(masteryLevelChange);
+        printer.println();
+        printer.boldOn();
+        printer.setSize('S');
+        printer.println(F("Number of Questions Correct"));
+        printer.boldOff();
+        printer.setSize('M');
+        printer.print(numCorrect);
+        printer.print("/");
+        printer.println(numAttempted);
+        printer.println();
+        printer.boldOn();
+        printer.setSize('S');
+        printer.println(F("Points Earned"));
+        printer.boldOff();
+        printer.setSize('M');
+        printer.println(pointsEarned);
+        printer.println();
+        printer.boldOn();
+        printer.setSize('S');
+        printer.println(F("Starting Mastery Level"));
+        printer.boldOff();
+        printer.setSize('M');
+        printer.println(startFPM);
+        printer.println();
+        printer.feed(2);
+        count = 1;
+      }
     }
   }
 }
